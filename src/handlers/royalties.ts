73f8995e7d0331d38e5896867564f9ed7f0063d7
Marko Marinovic <marinovic.marko@outlook.com>
2022-01-04 19:23:44 +0100

    add royalties to graph

---
+++ src/handlers/royalties.ts
@@ -0,0 +1,37 @@
+import { log } from '@graphprotocol/graph-ts';
+import {
+  RoyaltiesSetForCollection,
+  RoyaltiesSetForToken,
+} from '../../generated/RoyaltiesProvider/RoyaltiesProvider';
+import { NFT, NFTContract } from '../../generated/schema';
+import { getNFTId } from '../modules/nft';
+
+export function handleRoyaltiesSetForCollection(
+  event: RoyaltiesSetForCollection
+): void {
+  let nftContract = NFTContract.load(event.params.nftContract.toHex());
+  if (!nftContract) {
+    log.warning('NFT contract {} not available', [
+      event.params.nftContract.toHexString(),
+    ]);
+    return;
+  }
+
+  nftContract.royalties = event.params.fee;
+  nftContract.save();
+}
+
+export function handleRoyaltiesSetForToken(event: RoyaltiesSetForToken): void {
+  let nftId = getNFTId(
+    event.params.nftContract.toHexString(),
+    event.params.tokenId.toString()
+  );
+  let nft = NFT.load(nftId);
+  if (!nft) {
+    log.warning('NFT {} not available', [nftId]);
+    return;
+  }
+
+  nft.royalties = event.params.fee;
+  nft.save();
+}
