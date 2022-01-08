dd48c1ff3bad6ffa6f4225a555f27de73821a21a
Marko Marinovic <marinovic.marko@outlook.com>
2022-01-08 09:44:29 +0100

    update main subgraph

--- src/data/addresses.ts
+++ src/data/addresses.ts
@@ -5,7 +5,7 @@ export const Null = '0x0000000000000000000000000000000000000000';
 export function getEndemicMarketplaceAddress(): string {
   let network = dataSource.network();
   if (network == 'aurora') {
-    return '0x5f89c1bBbCAc22fc15aC3074c0CfeC6bcF117FE5';
+    return '0x915F1E2aDE42374BbdD9051BA84dc0EC401DBed2';
   }
 
   if (network == 'aurora-testnet') {
