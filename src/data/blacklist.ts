8e36106c345993abd2be609893d1941804ed23f6
Marko Marinovic <marinovic.marko@outlook.com>
2022-02-15 21:57:56 +0100

    add blacklisted colelction

--- src/data/blacklist.ts
+++ src/data/blacklist.ts
@@ -3,6 +3,7 @@ const blacklistedCollections = [
   '0xa5f39526d6c229f5f0e36f6239060d8c9b4d49e4', // cyber babies,
   '0x5bc73dfcbd6fc0305335b13dd4800c84e9950693', // shroomables goodies,
   '0x00860ef075a7f6719cad8d89433e3558c5d22dcc', // shroomables baddies,
+  '0xf18ca1c9e69e784f04aefaf806763eb85472c871', // che flies
 ];
 
 export { blacklistedCollections };
