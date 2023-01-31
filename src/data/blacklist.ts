81d2ca1ba1f1baf1f01abbcf301f7d6ba080339a
marko.perajica <marko.perajica@blankhq.co>
2023-02-01 00:01:12 +0100

    updated collections blacklist

--- src/data/blacklist.ts
+++ src/data/blacklist.ts
@@ -7,6 +7,18 @@ const blacklistedAuroraCollections = [
   '0x9f028079723735276102a0953f2debae7650dc50', // bored ape pro club
   '0x36904b103c7afc1cb983afcb94e1bd70e555c869', // ape king
   '0x414f674004f04273516b937b1459f8f99d478a6c', // skull man art
+  '0xa15c70a3cf8bce384c483946149f36a4f5c77f4e', // Alliaihe and the Fades
+  '0xad00fb5f1b7c7085919eb2bc07411c34bf6ddd10', // Crossoverse
+  '0x6d32a8ea13cac04769c17ff8f451c8ca998ba394', // Stones, Bones, and Dark Tones
+  '0xffd2d4af03f1d9397e98ff4ab68f977d130cabae', // Endemix
+  '0xe758e1da8c61cbd6837b76b932fb73db265149e1', // Null - KwADRA
+  '0xfe0e947a50c2dc8a569a6e4376e92d7ccb26b23d', // Dystopia
+  '0xbc07f5257cc2fbc815a5a75f8a6e4649f71d60d2', // Last Kuna Politician
+  '0xd6ceb3e7ba303b65bd08a24c02ac8559273d7d74', // Cool Cats Kitten Club
+  '0x4928f55aaa5a6e9392d53ebf82b1089a1fe161af', // Golden Super Rare
+  '0x821a4bed869515fd181dcc4cbe160521ba76a4ea', // The Kallé
+  '0x388e7c4b0b5b3c3795574c42472afdc461d4e670', // MULTIVERSE
+  '0x5f0b1817028424ef68954608402ee5cfa9650e55', // Neon Nova Dream Scene
 ];
 
 export { blacklistedAuroraCollections };
